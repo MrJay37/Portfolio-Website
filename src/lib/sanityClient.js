34d41298c19a7466907e934515b69efd8ef51124
Sanket Jain <djsj37@gmail.com>
2024-05-05 00:15:18 -0400

    Sanity credentials in environment variables

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -2,8 +2,8 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const sanityClient = createClient({
-    projectId: "evljjp3l",
-    dataset: "production",
+    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    dataset: process.env.REACT_APP_SANITY_PROJECT_DATASET,
     useCdn: true,
     apiVersion: "2023-01-01"
 })
